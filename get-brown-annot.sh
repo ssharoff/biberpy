ce42a573fa135f5393f0ab34e3b56b8c704ed16f
Serge Sharoff <serge.sharoff@gmail.com>
2018-12-10 14:14:23 +0000

    annotations updated

--- get-brown-annot.sh
+++ get-brown-annot.sh
@@ -1,1 +1,2 @@
-sed -r 's/^([A-C])/news\t\1\t\1/; s/^([D-F])/misc\t\1\t\1/; s/^([G-H])/nonfiction\t\1\t\1/; s/^(J)/academ\t\1\t\1/; s/^([K-R])/fiction\t\1\t\1/'
+# usually comes from a list brown corpus IDs
+sed -r 's/^([A-C])/news\t\1\t\1/; s/^([DH])/misc\t\1\t\1/; s/^([EFG])/nonfiction\t\1\t\1/; s/^(J)/academ\t\1\t\1/; s/^([K-R])/fiction\t\1\t\1/;s/^"ID"/"Top"\t"Brown"\t"ID"/'
